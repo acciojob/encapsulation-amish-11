7a4493f579c6b5cf41643f8b7ee5de4a56b8e03a
amish <amishahuja94@gmail.com>
2023-08-12 17:35:19 +0530

    assignment 2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly rw = new RWOnly();
+        //rw.name = "Amish";
+        //System.out.println(rw.name);
+        //'name' has private access in 'com.driver.RWOnly'
+        rw.setName("Amish");
+        System.out.println(rw.getName());
+    }
 }
